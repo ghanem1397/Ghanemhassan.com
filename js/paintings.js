4ee587c4a551df5b85015f7c19ee5a33a14f2e0c
ghanemhassan <thegeeart@gmail.com>
2026-05-31 20:43:17 +0200

    Add js/paintings.js

---
+++ js/paintings.js
@@ -0,0 +1,611 @@
+const PAINTINGS = [
+  {
+    "name": "A Silent War of the Mind",
+    "slug": "a-silent-war-of-the-mind",
+    "main": "https://drive.google.com/thumbnail?id=13ayRUYgWhjimuAmrigdJ2HDsMHFSi9NS&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 7500
+  },
+  {
+    "name": "Behind the crowds",
+    "slug": "behind-the-crowds",
+    "main": "https://drive.google.com/thumbnail?id=1XQfCKzBobjs4hwFGUQ_k9qsTBnKcEo7Y&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1A9jscyZ6gErMwW3Z6-RQ400BMC4zmzuX&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1KKWlC-o88YCDc72yBmXPzSACmmgT3j5T&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1aTzlfvb7Ve_VIF--NZWblSnIaBbemCAu&sz=w1200"
+    ],
+    "sold": false,
+    "price": 6500
+  },
+  {
+    "name": "Black Rose Whispers",
+    "slug": "black-rose-whispers",
+    "main": "https://drive.google.com/thumbnail?id=1e77m20-DGiAhULV_i-aUuHE9iuQ5GuEk&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 4900
+  },
+  {
+    "name": "City Dappled Dreams",
+    "slug": "city-dappled-dreams",
+    "main": "https://drive.google.com/thumbnail?id=1thk7OVNwwHpUtipnn4xv8RNLlTR5iG3_&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Dragons",
+    "slug": "dragons",
+    "main": "https://drive.google.com/thumbnail?id=1AshDdZ7g_0uyHPq8qcZeKNjRDagp8ooZ&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Embracing Life",
+    "slug": "embracing-life",
+    "main": "https://drive.google.com/thumbnail?id=13cWg5wl8lxmmzD71rKFYMtoNT_UNr-Lv&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1ysAcXJ2qqJupbAVy9-whHJzLFi1c9LId&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1ImoAO6Do8TiwoLjlZAn2T6RU0cCTHVcq&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "Gold Dust Dreams",
+    "slug": "gold-dust-dreams",
+    "main": "https://drive.google.com/thumbnail?id=1SXzdOJSBkO88g4-VIbgxIJAZtwniR8Dz&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 5100
+  },
+  {
+    "name": "Golden Enigma",
+    "slug": "golden-enigma",
+    "main": "https://drive.google.com/thumbnail?id=1_KJhMKA5vmW1Mm0kSkVr7RGCyjCxAemz&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1EOX2RKAu2fRCRo4XtTMkBAI87Fbw8ooO&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1dBQ8b2tqPcfxGHY5t9csuYR_DKaIEGnV&sz=w1200"
+    ],
+    "sold": false,
+    "price": 1800
+  },
+  {
+    "name": "In my veins 1",
+    "slug": "in-my-veins-1",
+    "main": "https://drive.google.com/thumbnail?id=1dR5QRHe-JBTfR8IZojQ2n-xLfMf9wGMg&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Inverno",
+    "slug": "inverno",
+    "main": "https://drive.google.com/thumbnail?id=1vTes8ktGaYG-uJ1vpD43zNUJMZ1RTxoU&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Kings and Queens",
+    "slug": "kings-and-queens",
+    "main": "https://drive.google.com/thumbnail?id=1X3yey8AA4L_r4uYAc_3ymd4p6ADz7RfH&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 600
+  },
+  {
+    "name": "Life",
+    "slug": "life",
+    "main": "https://drive.google.com/thumbnail?id=1_Gpi180ZIZ2RJE_4yTX3wacpic0Jh_me&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Lost",
+    "slug": "lost",
+    "main": "https://drive.google.com/thumbnail?id=1CUG42svNgWyP74xA7QPo3mt14zyUzM-k&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 3800
+  },
+  {
+    "name": "Luminous Eclipse",
+    "slug": "luminous-eclipse",
+    "main": "https://drive.google.com/thumbnail?id=1aNvstVGl8SvmNBaY0QGUs2BfJyJcaTxL&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Mosaic of life",
+    "slug": "mosaic-of-life",
+    "main": "https://drive.google.com/thumbnail?id=1ORGTVYI-4J9OzMs1nmi4YIoRasgVJ0lo&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1W_xONIkbLNxsxtzDcw-gZXHG242ykwtG&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1NVAczBcYf_zaFr_aDo2U3hlj0Y6nis6G&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "Mystic Gold",
+    "slug": "mystic-gold",
+    "main": "https://drive.google.com/thumbnail?id=16J44nymBOnVTns9FfbX3KKLQdmX-cnb1&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 600
+  },
+  {
+    "name": "Nabucco",
+    "slug": "nabucco",
+    "main": "https://drive.google.com/thumbnail?id=1z0vj1xsKvZ_LZCf57Jx4qK-0z9mKtqV7&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 1200
+  },
+  {
+    "name": "Paint The World",
+    "slug": "paint-the-world",
+    "main": "https://drive.google.com/thumbnail?id=1VWfN2iA8KEBV-c12oOPEBya19DT8nRJK&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Path of purpose",
+    "slug": "path-of-purpose",
+    "main": "https://drive.google.com/thumbnail?id=1gPMlckrexRj1cqRSykwd52573qm5AUX-&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1lZaBp96Cj9HYQhTACOeam6nzJSSV1Il8&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1ewmBKtJFXUv-kuj9431EEbl07fWDg7Yg&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1NxpEiWwLa6ovAf7DvPTHK_Ad-7R8kQ_c&sz=w1200"
+    ],
+    "sold": false,
+    "price": 2700
+  },
+  {
+    "name": "Pixels",
+    "slug": "pixels",
+    "main": "https://drive.google.com/thumbnail?id=1WfSpvW5DwcJLtQdl0xJ3ky_AV8E80-SB&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Sharp love",
+    "slug": "sharp-love",
+    "main": "https://drive.google.com/thumbnail?id=1_wOqpH22aE4W14uyQNvCEP5huAsZrQXt&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Shredded",
+    "slug": "shredded",
+    "main": "https://drive.google.com/thumbnail?id=1F6HxJu5AUuWmnUbfeDxTfCliTU3LdDvB&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1Bqs5mzAb28u1tyyglc1k5Z_TXsSWjnA4&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1gjs3Juzs9TLCBVB6BOW0wIBLRJxY2NzY&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "Skin of silence",
+    "slug": "skin-of-silence",
+    "main": "https://drive.google.com/thumbnail?id=1ltwHWcGO1QcV_5VkQNVqYaGnTfIV1jrd&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 26500
+  },
+  {
+    "name": "Sunsets",
+    "slug": "sunsets",
+    "main": "https://drive.google.com/thumbnail?id=1RFRg8QeR7olo_L_aUgwQPZwgr4ZYNsp3&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Sydney",
+    "slug": "sydney",
+    "main": "https://drive.google.com/thumbnail?id=12rvApz-WV2fKAKWFXrvY6x1IAD50cbfC&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1L3YAQ9jN0Jx4695qdHgMu0DUJA0yn9bT&sz=w1200"
+    ],
+    "sold": false,
+    "price": 3500
+  },
+  {
+    "name": "The Colorful Darkness",
+    "slug": "the-colorful-darkness",
+    "main": "https://drive.google.com/thumbnail?id=1Wk7j6nXxM06cZjRb94VESMtD7K6Uxg9p&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "The Growth Within",
+    "slug": "the-growth-within",
+    "main": "https://drive.google.com/thumbnail?id=1cfmO79KIT9INmiWMBU7nbdS2fhJAYtcT&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1Rl1wh_wefC0NoERyBwbjogDxHGKgsC_n&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1cOxat5KCW815zvVSAC7B6AqQziTfh2WN&sz=w1200"
+    ],
+    "sold": false,
+    "price": 2500
+  },
+  {
+    "name": "The Healing Wound",
+    "slug": "the-healing-wound",
+    "main": "https://drive.google.com/thumbnail?id=1s1sguGOtAxm4JlK55B9vTkxKi52ux-BX&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 10200
+  },
+  {
+    "name": "The Holy Night",
+    "slug": "the-holy-night",
+    "main": "https://drive.google.com/thumbnail?id=1AWZB9HVAipTI6aMnGW5W0vLiMSGJb86N&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "The Memory Beneath",
+    "slug": "the-memory-beneath",
+    "main": "https://drive.google.com/thumbnail?id=1F3Na6b9x_c-F8bhNQU4g2RKHj_qHcCAd&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 13300
+  },
+  {
+    "name": "Tokyo",
+    "slug": "tokyo",
+    "main": "https://drive.google.com/thumbnail?id=1rjGKD4CjI_S_RFe22ySRMobhfiPsRop8&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "Winds of Change",
+    "slug": "winds-of-change",
+    "main": "https://drive.google.com/thumbnail?id=18j0MTBisum3fBGKnKoMJyScPOVXu5Odp&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1LpoqYGqDteyxQm5J9MX0r49KgUKYOAYx&sz=w1200"
+    ],
+    "sold": false,
+    "price": 8600
+  },
+  {
+    "name": "ambler",
+    "slug": "ambler",
+    "main": "https://drive.google.com/thumbnail?id=1ZNEwABExYjLk2VqILuHd_ZUBcvg_v338&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "aristocrates",
+    "slug": "aristocrates",
+    "main": "https://drive.google.com/thumbnail?id=1k9xYm5fgyLk5qneteOuvTdvuHq1y2CHj&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "august",
+    "slug": "august",
+    "main": "https://drive.google.com/thumbnail?id=1eSwEQvnRFmPotK5IbUlBVcIvmlomCyrc&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "bound of love",
+    "slug": "bound-of-love",
+    "main": "https://drive.google.com/thumbnail?id=1EnLmya6wqKW44KfJZpWmvDJOPEffV-AG&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "broken mirrors",
+    "slug": "broken-mirrors",
+    "main": "https://drive.google.com/thumbnail?id=14OM0SbHOL_gqg0Clv6CV2LwsSJ2wk97a&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "celebration of identity",
+    "slug": "celebration-of-identity",
+    "main": "https://drive.google.com/thumbnail?id=1jzbzpgwOiPX5lj7t1qiCX4qZF897qcXH&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=18474W3m8wu_kf9_e2gDjtyHEN0raxW5J&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1hqZQVCTpR4vxM-dDYe_asJTqUP3GeyUC&sz=w1200"
+    ],
+    "sold": false,
+    "price": 10700
+  },
+  {
+    "name": "chromatic rush",
+    "slug": "chromatic-rush",
+    "main": "https://drive.google.com/thumbnail?id=14LR5L8uZNleh0vga1qujXFd4sC1aBids&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 4700
+  },
+  {
+    "name": "circles of life",
+    "slug": "circles-of-life",
+    "main": "https://drive.google.com/thumbnail?id=1GkPpaY8Hwa75kZs-cKhJGyRiSwpn9Yjh&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "dilerium",
+    "slug": "dilerium",
+    "main": "https://drive.google.com/thumbnail?id=1NHk2iAPU928BqgBX2L5St6oiIJ9Adio3&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "edwina",
+    "slug": "edwina",
+    "main": "https://drive.google.com/thumbnail?id=1UNOQnDPlhEY0deRUoQ8GF36VTd6g3nWa&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "egypt",
+    "slug": "egypt",
+    "main": "https://drive.google.com/thumbnail?id=1Pa5-99HC9VE8uy56C7QWS1MbLimQQ8Id&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1nB6LlxoKyO-593afI9IOQ9afxDLFR3zq&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1xsK9gWk6texBfUIUNwpllGQv-WEdIlJW&sz=w1200"
+    ],
+    "sold": false,
+    "price": 4000
+  },
+  {
+    "name": "escape",
+    "slug": "escape",
+    "main": "https://drive.google.com/thumbnail?id=1aTV3BC1K0HSRVyvn0uRPKw0R7Eek2uDb&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "everywhere we go",
+    "slug": "everywhere-we-go",
+    "main": "https://drive.google.com/thumbnail?id=1LEKs_5_DGJ2jG8GlQ2mamwwmSnX2KtfF&sz=w1200",
+    "extras": [],
+    "sold": true
+  },
+  {
+    "name": "forbidden fruit",
+    "slug": "forbidden-fruit",
+    "main": "https://drive.google.com/thumbnail?id=104AEAHfdFckX1NKlc1hofLbocIX3dgSC&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "gemini",
+    "slug": "gemini",
+    "main": "https://drive.google.com/thumbnail?id=1PBeN8NrAxpXjB5z09rJxS76aQ2_N3CWG&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1KZyK7RD-Ryj9Q7-Y0Hq6G3nGfbqQbbKP&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "gilded tale",
+    "slug": "gilded-tale",
+    "main": "https://drive.google.com/thumbnail?id=1VwEv2C62Y_bVqyPyH7E0kSWcpwakXgD3&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1u2nkVG_iaqfV6LtlIknqQIJCoE9dlTkA&sz=w1200"
+    ],
+    "sold": false,
+    "price": 9800
+  },
+  {
+    "name": "golden riddles",
+    "slug": "golden-riddles",
+    "main": "https://drive.google.com/thumbnail?id=1NjKF0xCJ7uPtNfI3QcBx91CZchDzoS4g&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=13DHyGww85sxQlxSXtDHnwNdmnuygH0ys&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "growing together",
+    "slug": "growing-together",
+    "main": "https://drive.google.com/thumbnail?id=1X9MporS6QNiN_DZRoBkXzTlgfbK-DM2u&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 5500
+  },
+  {
+    "name": "harmony of the cosmos",
+    "slug": "harmony-of-the-cosmos",
+    "main": "https://drive.google.com/thumbnail?id=1OjMyfi9kkZziQgur-f-j9DmZwAH8AHND&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1YmQaiqdzffDvd5UUW0tn9LdKYpq4CebT&sz=w1200"
+    ],
+    "sold": false,
+    "price": 4400
+  },
+  {
+    "name": "hidden figures",
+    "slug": "hidden-figures",
+    "main": "https://drive.google.com/thumbnail?id=1dwVO0ForiEX3mMnxKDGylhvxx4apUXMf&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "lion king",
+    "slug": "lion-king",
+    "main": "https://drive.google.com/thumbnail?id=1oEuEWCqk5OllQTxz9kfrET33WErLoGxY&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "map of dreams",
+    "slug": "map-of-dreams",
+    "main": "https://drive.google.com/thumbnail?id=1pWPlffLzw-0xdm96KtnIKTL7egGFfsjF&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 7500
+  },
+  {
+    "name": "my id",
+    "slug": "my-id",
+    "main": "https://drive.google.com/thumbnail?id=1bXqn-j2MbXU3S2rCgIXX_BJtYHI_vGfO&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "newyork",
+    "slug": "newyork",
+    "main": "https://drive.google.com/thumbnail?id=1STzO1ZRcmVlxiNJp2Yr7Rtce2bacIqtq&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1rSu7I5DfrlQmR0nnGkvMlbe-h-glYAKM&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "red lips",
+    "slug": "red-lips",
+    "main": "https://drive.google.com/thumbnail?id=17nbY1nE-Rda-6Qt56Ihb8PcuJ4h56iD3&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1Ora5l9Mpig_mvUpmSF_TUOV9CpCac9hP&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1wFHNH06s8BMW3v6k1iTRHSgnZuuIZE-V&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "shadows",
+    "slug": "shadows",
+    "main": "https://drive.google.com/thumbnail?id=1VqNly5mjDl-rRiTmzVmpk9XGPEt3QSvu&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1OdX4csus6O5TPv4Xxe8ssdrwdAne7Ayp&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "sharp blades of love",
+    "slug": "sharp-blades-of-love",
+    "main": "https://drive.google.com/thumbnail?id=1mINkJMVDLuNr9HiDzUliXDfT4721J__Y&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "she who carries the lights",
+    "slug": "she-who-carries-the-lights",
+    "main": "https://drive.google.com/thumbnail?id=1NRQhIZvIxdwG2wyegb4RCFW1Phu_yV7J&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1yTIzgU2w6OqY3TaBK8_UWHtZYx1IrZsq&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1ppsNKkiDosOJl3awRerIREzAu6qqR3Kt&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1050Z4jhpOkvDVvkdDUZqpGOnGC3WMXzN&sz=w1200",
+      "https://drive.google.com/thumbnail?id=19HhCUEkbMVhnvfXoat4t-pioAcL8bXoe&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1E1lrP5qU4WXNE4m3AW-Q5kNLgIPD2L_-&sz=w1200"
+    ],
+    "sold": false
+  },
+  {
+    "name": "sky full of stars",
+    "slug": "sky-full-of-stars",
+    "main": "https://drive.google.com/thumbnail?id=1M-RoQcmdEi9EWH-hfB8x_aM5vwaAp8SL&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "subways",
+    "slug": "subways",
+    "main": "https://drive.google.com/thumbnail?id=18cc34MndoeD6I2AqYEabSvGrRLKRq0oy&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "the four elements",
+    "slug": "the-four-elements",
+    "main": "https://drive.google.com/thumbnail?id=1WylvnWXLipLMbYkAfELd7v1vL0fQ55Kc&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1lIcZjpGjLxAyFmb7qP-PwdoJ5fYiW3wh&sz=w1200"
+    ],
+    "sold": false,
+    "price": 7600
+  },
+  {
+    "name": "the four virtues",
+    "slug": "the-four-virtues",
+    "main": "https://drive.google.com/thumbnail?id=13n4Dk2QVGSY8P96Qq7G0-JfByQZwiL9O&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 4900
+  },
+  {
+    "name": "the lamentable lady",
+    "slug": "the-lamentable-lady",
+    "main": "https://drive.google.com/thumbnail?id=1OtB1uAJmaKYV7lRVsWAhdxiCJ0ZD180g&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "the last tree",
+    "slug": "the-last-tree",
+    "main": "https://drive.google.com/thumbnail?id=1RdWdcqxMrdXKiX63OKCPhCd2XiWV2VeG&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "the long wait",
+    "slug": "the-long-wait",
+    "main": "https://drive.google.com/thumbnail?id=1mGe9-PxKPJEESx9b8Bn6Ds4In7S2QdG8&sz=w1200",
+    "extras": [],
+    "sold": false,
+    "price": 3800
+  },
+  {
+    "name": "the silent night",
+    "slug": "the-silent-night",
+    "main": "https://drive.google.com/thumbnail?id=1dXgQysCOXa0Jr7okYlJmxHHqT-SUrET6&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "the weight of asking",
+    "slug": "the-weight-of-asking",
+    "main": "https://drive.google.com/thumbnail?id=1dqQkdYWZ8iRBQDBRIh7H01BXLwjopxdE&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1yxxPceQGwVV4e4iYBlb9vVqJn6Fic0pV&sz=w1200"
+    ],
+    "sold": false,
+    "price": 9200
+  },
+  {
+    "name": "traces",
+    "slug": "traces",
+    "main": "https://drive.google.com/thumbnail?id=1B2mRXoXti4XLFkG5JRXrpty1X8lJN3Iy&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "trip dance",
+    "slug": "trip-dance",
+    "main": "https://drive.google.com/thumbnail?id=1-uxxcs5P70gNtKW2UcUkB7ZCqVbXv1BQ&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "universe",
+    "slug": "universe",
+    "main": "https://drive.google.com/thumbnail?id=1lXZNY6gu9N3a_yQbBEEG6dI1Qq-4g1MM&sz=w1200",
+    "extras": [],
+    "sold": false
+  },
+  {
+    "name": "veild emotions",
+    "slug": "veild-emotions",
+    "main": "https://drive.google.com/thumbnail?id=1EafKSUq7QjngUyt4k6RkSR9WTyUzOZVk&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1ncQEAthKrxwvc3dUUepwhBiO9D3vJszL&sz=w1200"
+    ],
+    "sold": false,
+    "price": 7400
+  },
+  {
+    "name": "web of colors",
+    "slug": "web-of-colors",
+    "main": "https://drive.google.com/thumbnail?id=1h9B4RAf9ShHk2PmHfcBt_kgcvlI4-zgy&sz=w1200",
+    "extras": [
+      "https://drive.google.com/thumbnail?id=1n45IGmcxoYFU3rQrstdqLfT0SM_DMJSU&sz=w1200",
+      "https://drive.google.com/thumbnail?id=1M8YN5sY-KAB7KZcjR7lK1HHwdcudGEBz&sz=w1200"
+    ],
+    "sold": false
+  }
+];
